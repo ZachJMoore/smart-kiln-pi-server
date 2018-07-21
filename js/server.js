94157713a8e12e43e7a6643dca4e5e2cc9995f2a
ZachMoore <Get3Moore@gmail.com>
2018-07-20 22:01:36 -0400

    Reorganize subdomain requests

--- js/server.js
+++ js/server.js
@@ -7,32 +7,73 @@ const config = require("./static/config");
 const fetch = require("node-fetch")
 const socketTunnel = require("./socket-tunnel/lib/api")
 
+let generateUUID = () => { // Public Domain/MIT
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+let requestTunnel = (random)=>{
+    return socketTunnel.connect("https://smartkiln.xyz", `${random ? generateUUID() : config.uuid}`, port)
+}
+
 waitForLogin().then(()=>{
 
     const server = app.listen(port, console.log(new Date() + ": server running on port " + port));
 
-    socketTunnel.connect("https://smartkiln.xyz", `${config.uuid}`, port)
-        .then(url =>{
+    requestTunnel()
+        .then((url)=>{
             console.log(url)
             userDB.updateKiln(url)
         })
-        .catch(err=>{
-            console.log(err);
-            socketTunnel.connect("https://smartkiln.xyz", `${config.uuid}-${Math.floor(Math.random()*10)}`, port)
-                .then(url =>{
+        .catch((err)=>{
+            console.log(err)
+            requestTunnel(true)
+                .then((url)=>{
                     console.log(url)
                     userDB.updateKiln(url)
                 })
-                .catch(err=>{
-                    console.log(err);
-                    userDB.updateKiln({
-                        url: "",
-                        error: {
-                            message: "Could not establish a tunnel to the kiln",
-                            serverMessage: err
-                        }
-                    })
+                .catch((err)=>{
+                    console.log(err)
+                    requestTunnel(true)
+                        .then((url)=>{
+                            console.log(url)
+                            userDB.updateKiln(url)
+                        })
+                        .catch((err)=>{
+                            console.log(err)
+                        })
                 })
         })
+
+    // socketTunnel.connect("https://smartkiln.xyz", `${config.uuid}`, port)
+    //     .then(url =>{
+    //         console.log(url)
+    //         userDB.updateKiln(url)
+    //     })
+    //     .catch(err=>{
+    //         console.log(err);
+    //         socketTunnel.connect("https://smartkiln.xyz", `${config.uuid}-${Math.floor(Math.random()*10)}`, port)
+    //             .then(url =>{
+    //                 console.log(url)
+    //                 userDB.updateKiln(url)
+    //             })
+    //             .catch(err=>{
+    //                 console.log(err);
+    //                 userDB.updateKiln({
+    //                     url: "",
+    //                     error: {
+    //                         message: "Could not establish a tunnel to the kiln",
+    //                         serverMessage: err
+    //                     }
+    //                 })
+    //             })
+    //     })
 
 })
